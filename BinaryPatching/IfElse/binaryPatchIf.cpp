1313d407038bca27bfd258dc283cb1a93a39ef93
William A-G <william.a-g@hotmail.com>
2019-05-03 09:00:33 -0400

    Binary Is Patched
    * Binary Patched

--- BinaryPatching/IfElse/binaryPatchIf.cpp
+++ BinaryPatching/IfElse/binaryPatchIf.cpp
@@ -1,15 +1,26 @@
 #include <stdio.h>
 
+void neverPrint()
+{
+	printf("This should never print!\n");
+}
+
+void alwaysPrint()
+{
+	printf("This should always print!\n");
+}
+
 int main()
 {
     // Interesting: Compiler optimises by removing any statement that are know to always be false on compile time
     int value = 0;
     if(value == 1)
     {
-        printf("This should never print!\n");
-    }
+        neverPrint();
+	}
     else
     {
-        printf("This should always print!\n");
+        alwaysPrint();
     }
 }
+
